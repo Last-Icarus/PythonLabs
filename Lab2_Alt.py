faf43222efbcb72fa4a1ddc07c11e63eea9a59c8
Michael Nikitin <96845302+Last-Icarus@users.noreply.github.com>
2022-06-22 18:46:08 +0300

    Add files via upload

---
+++ Lab2_Alt.py
@@ -0,0 +1,13 @@
+while True:                            # Чтобы каждый раз не перезапускать программу
+    string = input()
+    if string.lower() == 'break':
+        break
+    
+    a = ''
+    for i in range(len(string)):
+        if i != 2 and i != len(string)-1:
+            a+=string[i]
+    string = string.lower()
+    if string.find('с') != -1 or string.find('c') != -1:
+        print("В строке есть символ С")
+    print(a)
